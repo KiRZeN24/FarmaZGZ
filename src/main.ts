75a7e48e127da7d8af929bca270856160091be11
KiRZeN24 <carlosrufo92@gmail.com>
2025-09-14 12:12:44 +0200

    config: add validation pipe and API prefix to main setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT ?? 3001);
   console.log(
     `FarmaZGZ Backend running on http://localhost:${process.env.PORT ?? 3001}`,
   );
