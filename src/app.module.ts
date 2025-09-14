84aa9f9cc8391e3e1d8a31b5316bbf05275b88c3
KiRZeN24 <carlosrufo92@gmail.com>
2025-09-14 11:43:35 +0200

    feat: create pharmacies module and integrate with app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PharmaciesModule } from './pharmacies/pharmacies.module';
 
 @Module({
-  imports: [],
+  imports: [PharmaciesModule],
   controllers: [AppController],
   providers: [AppService],
 })
