fc910d24aaef9cce3218a637404350a2477ae2ff
KiRZeN24 <carlosrufo92@gmail.com>
2025-09-29 18:34:53 +0200

    feat(database): integrate PostgreSQL with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PharmaciesModule } from './pharmacies/pharmacies.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PharmaciesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') !== 'production',
+        ssl: { rejectUnauthorized: false },
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+    PharmaciesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
